f8081f332428e4c9e73b286d7c4631cefd8335c2
Tung <tungnd-0844>
2020-06-16 17:20:33 +0700

    config

--- src/main/kotlin/com/example/BookReviewingSys/BookReviewingSysApplication.kt
+++ src/main/kotlin/com/example/BookReviewingSys/BookReviewingSysApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BookReviewingSysApplication
+open class BookReviewingSysApplication
 
 fun main(args: Array<String>) {
 	runApplication<BookReviewingSysApplication>(*args)
